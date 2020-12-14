0237a222c5042c3e0d6b90a914b5ba1e8f3f6bd3
Fernando Cagua <efcaguab@gmail.com>
2020-12-14 21:37:54 +1300

    Clean boat data from PESKADAT

--- R/clean-peskadat.R
+++ R/clean-peskadat.R
@@ -4,3 +4,15 @@ clean_peskasdat_species <- function(path){
     readr::read_csv() %>%
     dplyr::mutate(species_code = as.character(species_code))
 }
+
+
+clean_peskasdat_boats <- function(path){
+  path %>%
+    readr::read_csv(col_types = readr::cols(.default = readr::col_character())) %>%
+    janitor::clean_names() %>%
+    dplyr::mutate(imei_long = imei,
+                  installation_date = dplyr::if_else(stringr::str_detect(installation_date, "/[0-9]{2}$"),
+                                                      lubridate::mdy(installation_date),
+                                                      lubridate::dmy(installation_date)))
+
+}
